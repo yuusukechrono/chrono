3776773d40b0e191076d3d75cc985e3b6b6da4e4
yuusukechrono <53998825+yuusukechrono@users.noreply.github.com>
2019-09-25 00:50:23 +0900

    Add files via upload

--- intro-ruby/test.rb
+++ intro-ruby/test.rb
@@ -1,3 +1,28 @@
+def median(a,b,c)
+  candidate = [a,b,c]
+  j = candidate.length
+
+for z in 0..j-2 do
+  i=0
+  for i in 0..j-2 do
+    if candidate[i] > candidate[i+1]
+      x = candidate[i]
+      candidate[i] = candidate[i+1]
+      candidate[i+1] = x
+    else
+    end
+  end
+end
+ puts(candidate[(j-1)/2])
+end
+
+puts(median(5,7,2))
+puts(median(7,3,2))
+puts(median(1,4,2))
+
+
+
+
 def bigger(a,b)
   if a>b
     a
@@ -14,7 +39,7 @@ def smaller(a,b)
   end
 end
 
-def median(a,b,c)
+def median2(a,b,c)
  candidate=[a,b,c]
 
  candidate_bigger = bigger(a,b)
@@ -27,11 +52,10 @@ def median(a,b,c)
  candidate
 end
 
-puts(median(5,7,2))
-puts(median(7,3,2))
-puts(median(1,4,2))
 
-def median2(a,b,c)
+
+
+def median1(a,b,c)
  if a > b
    if a > c
      if b > c
