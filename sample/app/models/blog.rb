b7cf33e88987ea4b88b142d6ceff5ee31cd1ee46
yusuke nakano <yuusukechrono@gmail.com>
2019-11-24 14:03:22 +0000

    modify varification

--- sample/app/models/blog.rb
+++ sample/app/models/blog.rb
@@ -1,3 +1,6 @@
 class Blog < ApplicationRecord
   validates :title, presence: true
+  validates :title, length: { in:1..20}
+  validates :content, presence: true
+  validates :content, length: { in:1..140}
 end
